a1a7dbd9d18320254de0cce073d85bde092dcb77
SwanHub <jdprince555@gmail.com>
2019-07-16 17:50:16 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,8 @@
 class Book
+  attr_accessor :genre
+  attr_accessor :author
+  attr_accessor :page_count
+
   def initialize(title)
     @title = title
   end
@@ -7,36 +11,8 @@ class Book
     @title
   end
 
-  def author
-    @author
-  end
-
-  def author=(author_name)
-    @author = author_name
-  end
-
-  def page_count
-    @page_count
-  end
-
-  def page_count=(page_count_number)
-    @page_count = page_count_number
-  end
-
-  def genre
-    @genre
-  end
-
-  def genre=(genre_type)
-    @genre = genre_type
-  end
-
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
-
-
 end
-
-book = Book.new("And Then There Were None")
